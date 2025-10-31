2e55844fd5abae6f236327f7f87d664d890c2456
liala-2003 <liaabuohaseba@gmail.com>
2025-10-31 21:52:01 +0200

    add notfound

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -4,9 +4,9 @@ export default function NotFound() {
   return (
     <>
     <div className="d-flex align-items-center justify-content-center flex-column vh-100 bg-dark">
-      <h1 className="fa-fade display-1 fw-bold text-danger mb-3">404</h1>
-      <p className="fs-3 text-white text-uppercase">System Error</p>
-      <p className="text-secondary mb-4">We can’t seem to find that page.</p>
+      <h1 className="fa-fade display-1 fw-bold text-danger mb-3">مرحبا</h1>
+      <p className="fs-3 text-white text-uppercase">هذا ايرووو</p>
+      <p className="text-secondary mb-4">لا توجد هذه الصفحه من ضمن الصفحات المتاحه.</p>
       {/* <Link to="/" className="btn btn-danger px-4 py-2">Go Home</Link> */}
     </div>
 
