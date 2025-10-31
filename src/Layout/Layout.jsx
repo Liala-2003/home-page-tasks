5ce38d9d6bb5876691b5a39fc3a2f020856c8d40
liala-2003 <liaabuohaseba@gmail.com>
2025-10-31 23:10:03 +0200

    akkk

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -14,7 +14,7 @@
 // }
 
 import { Outlet } from 'react-router-dom';
-import Navbar from '../Components/Navbar/Navbar.jsx';
+import Navbar from '../components/navbar/navbar.jsx';
 
 
 function Layout() {
