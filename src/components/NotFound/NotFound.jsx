e4e227c3123e8c92b3692a7d5c67961e2abe003d
liala-2003 <liaabuohaseba@gmail.com>
2025-11-01 15:16:08 +0200

    add notfound

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -7,7 +7,7 @@ export default function NotFound() {
       <h1 className="fa-fade display-1 fw-bold text-danger mb-3">مرحبا</h1>
       <p className="fs-3 text-white text-uppercase">هذا ايرووو</p>
       <p className="text-secondary mb-4">لا توجد هذه الصفحه من ضمن الصفحات المتاحه.</p>
-      {/* <Link to="/" className="btn btn-danger px-4 py-2">Go Home</Link> */}
+      <Link to="/" className="btn btn-dark px-4 py-2">Go Home</Link>
     </div>
 
     </>
